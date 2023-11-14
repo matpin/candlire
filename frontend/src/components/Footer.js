12f2f4ce89c1dd4e1e706da9f64c5f2c2fa3d657
matpin <stamkouk92@gmail.com>
2023-11-14 16:20:21 +0200

    Adding styles

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import "./Footer.css";
+import { Link } from 'react-router-dom';
+import logo from "../../src/candlirelogo.png";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Footer() {
   return (
-    <div className="footerContainer">Footer</div>
+    <div className="footerContainer">
+      <div className="titleLogoContainer">
+      <img
+          className="footerLogo"
+          src={logo}
+          alt="candlireImage"
+        />
+      <Link className="footerTitle" to="/">Candlire</Link>
+      </div>
+      <div className="socialMedia">
+      <FacebookIcon style={{color: "#333", fontSize: "30"}} />
+      <InstagramIcon style={{color: "#333", fontSize: "30"}} />
+      </div>
+      <p className="footerCopyrights">Candlire &copy; {new Date().getFullYear()} All rights reserved.</p>
+    </div>
   )
 }
 
