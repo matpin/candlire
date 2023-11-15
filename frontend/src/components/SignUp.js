4e5299ee19d45f69b792e99fb753fa240b70f98e
matpin <stamkouk92@gmail.com>
2023-11-15 12:39:46 +0200

    Adding styles to sign up component

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Alert from '@mui/material/Alert';
+import Alert from "@mui/material/Alert";
+import "./SignUp.css";
+import logo from "../../src/signInUpImage.jpg";
+import Button from "@mui/material/Button";
 
 function SignUp() {
   const [username, setUsername] = useState("");
@@ -36,49 +39,77 @@ function SignUp() {
   }
 
   return (
-    <div>
-      <h2>Sign up to Candlire</h2>
-      <form>
-        <div>
-          <label>Username</label>
-          <input
-            type="text"
-            onChange={(e) => setUsername(e.target.value)}
-            value={username}
-            spellCheck={false}
-          />
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            spellCheck={false}
-          />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
-        </div>
-        <div>
-          <label>Confirm Password</label>
-          <input
-            type="password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            value={confirmPassword}
-          />
-          {password !== confirmPassword ? (
-            <Alert severity="error">Passwords does not match</Alert>
-          ) : ("")} 
-        </div>
-        <button onClick={handleSignUp}>Create Account</button>
-        <p>Already have an account? <Link to="/signin">Sign In</Link></p>
-      </form>
+    <div className="signUpContainer">
+      <div className="signUpLeftSide">
+        <img className="signInUpImage" src={logo} alt="signInUpImage" />
+      </div>
+      <div className="signUpRightSide">
+        <h2 className="signUpTitle">Sign up to Candlire</h2>
+        <form className="signUpFormContainer">
+          <div className="signUpUsernamePassword">
+            <label>Username</label>
+            <input
+              type="text"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+              spellCheck={false}
+              className="signUpInput"
+            />
+          </div>
+          <div className="signUpUsernamePassword">
+            <label>Email</label>
+            <input
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              spellCheck={false}
+              className="signUpInput"
+            />
+          </div>
+          <div className="signUpUsernamePassword">
+            <label>Password</label>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              className="signUpInput"
+            />
+          </div>
+          <div className="signUpUsernamePassword">
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              value={confirmPassword}
+              className="signUpInput"
+            />
+            {password !== confirmPassword ? (
+              <Alert severity="error">Passwords does not match</Alert>
+            ) : (
+              ""
+            )}
+          </div>
+          <Button
+            onClick={handleSignUp}
+            size="medium"
+            style={{
+              backgroundColor: "#000",
+              color: "#fff",
+              borderRadius: "50px",
+              height: "5vh",
+            }}
+            variant="filled"
+          >
+            Create Account
+          </Button>
+        </form>
+        <p className="existedAccount">
+          Already have an account?{" "}
+          <Link className="signInLink" to="/signin">
+            Sign In
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
