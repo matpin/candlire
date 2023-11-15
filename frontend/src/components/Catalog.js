7e35a6c323d2a87e02724658fd122e4f15766ee6
matpin <stamkouk92@gmail.com>
2023-11-15 12:14:18 +0200

    Adding order by price on catalog component

--- frontend/src/components/Catalog.js
+++ frontend/src/components/Catalog.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ProductsList from './ProductsList';
+import React, { useState } from "react";
+import ProductsList from "./ProductsList";
 
 function Catalog({ productsArray }) {
+  const [priceOrder, setPriceOrder] = useState("desc");
+  let sortedProductsArray = productsArray;
+
+  function handleOrder(e) {
+    console.log(e.target.value);
+    setPriceOrder(e.target.value)
+    if (priceOrder === "asc") {
+      sortedProductsArray = productsArray.sort((a, b) => b.price - a.price);
+    } else {
+      sortedProductsArray = productsArray.sort((a, b) => a.price - b.price);
+    }
+    console.log(sortedProductsArray);
+  }
+
   return (
     <div>
-        <ProductsList productsArray={productsArray} />
+      <div>
+        <select value={priceOrder} onChange={handleOrder}>
+          <option value="">--Sort by--</option>
+          <option value="asc">Price(Low to High)</option>
+          <option value="desc">Price(High to Low)</option>
+        </select>
+      </div>
+      <ProductsList productsArray={sortedProductsArray} />
     </div>
-  )
+  );
 }
 
 export default Catalog;
