9894737d3869c240bcedc617c56e05870b33f9a4
matpin <stamkouk92@gmail.com>
2023-11-20 18:06:41 +0200

    Minor changes on footer component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -16,6 +16,10 @@ function Footer() {
         />
       <Link className="footerTitle" to="/">Candlire</Link>
       </div>
+      <div className="footerDetails">
+        <Link to="/" className="footerLinks">Home</Link>
+        <Link to="aboutus" className="footerLinks">About us</Link>
+      </div>
       <div className="socialMedia">
       <FacebookIcon style={{color: "#333", fontSize: "30"}} />
       <InstagramIcon style={{color: "#333", fontSize: "30"}} />
