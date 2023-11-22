71285de06078e4b64aae8cec656976d3736a665e
matpin <stamkouk92@gmail.com>
2023-11-22 13:42:53 +0200

    Adding styles on page not found component

--- frontend/src/components/PageNotFound.js
+++ frontend/src/components/PageNotFound.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import "./PageNotFound.css";
 
 function PageNotFound() {
   return (
-    <div>
-        <h1>404</h1>
-        <h1>Page not found.</h1>
+    <div className="pageNotFoundContainer">
+        <h1 className="pageNotFoundHead">404</h1>
+        <h1 className="pageNotFound">Page not found.</h1>
     </div>
   )
 }
